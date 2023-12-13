8f14e12d2ae90da6b6a7eb492c755f4c267fd303
Maksim Ramanau <mramanau@iba.by>
2023-12-13 12:11:09 +0300

    feat: implement pagination, update api requests

--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -1,37 +1,58 @@
-import React from 'react'
-import { useAppSelector } from '../hooks/redux'
+import React, {useEffect, useState} from 'react'
+import {useAppSelector} from '../hooks/redux'
+import styles from "./homePage/styles.module.css";
+import {BookCard} from "../components/bookCard";
+import {useLazyGetBookInfoQuery} from "../store/itbook/itbook.api";
+import ReactPaginate from "react-paginate";
 
 export function FavouritesPage() {
-   const {favourites} = useAppSelector(state => state.itbook)
-console.log(favourites)
-   if(favourites.length === 0) return <p className='text-center'>No items.</p>
+    const [itemOffset, setItemOffset] = useState(0);
+    const {favourites} = useAppSelector(state => state.itbook)
+    const [fetchFavBooks, {isLoading: isFavLoading, data: books}] = useLazyGetBookInfoQuery()
 
-//    const removeFavourites = (url: string) => {
-//      const index = favourites.indexOf(url)
-//      if (index === -1) {
-//      favourites.splice(index, 1)
-//      }
-//      return favourites
-//    }
-//    const clickHandler = (url: string) => {
-//     removeFavourites(url)
-//    // setDropdown(false)
-// }
+    const itemsPerPage = 5;
+    const endOffset = itemOffset + itemsPerPage;
+    const currentItems = books?.slice(itemOffset, endOffset) ?? [];
+    const pageCount = Math.ceil((books?.length ?? 1) / itemsPerPage);
 
- return (
-   <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
-    <ul className='list-none'>
-    { favourites.map(f => 
-      <li key={f}>
-         <a href={f} target='_blank'>{f}</a>
+    useEffect(() => {
+        fetchFavBooks({books: favourites})
+    }, [])
 
-         {/* <button 
-               className='py-2 px-3 bg-red-400 rounded hover:shadow-md transition-all'
-              //  onClick={(element) => clickHandler(f)}
-            >Remove</button> */}
-      </li>
-      )}
-    </ul>
-    </div>
- )
+    const handlePageClick = (event: any) => {
+        const newOffset = (event.selected * itemsPerPage) % (books?.length ?? 1);
+        setItemOffset(newOffset);
+    };
+
+    return (
+        <>
+            <ol className={styles.booksList}>
+                {isFavLoading && <p className='text-center'>Loading...</p>}
+                {currentItems?.map(book => (
+                    <li key={book?.isbn13}
+                        className='py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer'>
+                        <BookCard
+                            title={book?.title}
+                            subtitle={book.subtitle}
+                            price={book.price}
+                            isbn13={book.isbn13}
+                            url={book.url}
+                            image={book.image}
+                        />
+                    </li>
+                ))}
+            </ol>
+            <ReactPaginate
+                previousLabel={"← Previous"}
+                nextLabel={"Next →"}
+                pageCount={pageCount}
+                onPageChange={handlePageClick}
+                containerClassName={styles.pagination}
+                previousLinkClassName={styles.pagination__link}
+                nextLinkClassName={styles.pagination__link}
+                disabledClassName={styles.pagination__link__disabled}
+                activeClassName={styles.pagination__link__active}
+            />
+        </>
+    )
 }
--- src/store/itbook/itbook.api.ts
+++ src/store/itbook/itbook.api.ts
@@ -1,27 +1,40 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import { IBook, IBookInfo, ServerResponse } from '../../models/models'
 
+const url = 'https://api.itbook.store/1.0/'
+
 export const itbookApi = createApi({
     reducerPath: 'itbook/api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://api.itbook.store/1.0/'
+        baseUrl: url
     }),
     refetchOnFocus: true,
     endpoints: build => ({
-        searchBooks: build.query<IBook[], string>({
-            query: (search: string) => ({
-                url: `search/${search}`,      
+        searchBooks: build.query<ServerResponse<IBook>, { value: string; page: number }>({
+            query: ({ value, page }) => ({
+                url: `search/${value}/${page}`,
+            }),
+            transformResponse: (response: ServerResponse<IBook>) => response
+        }),
+        searchNewBooks: build.query<IBook[], void>({
+            query: () => ({
+                url: `new`
             }),
             transformResponse: (response: ServerResponse<IBook>) => response.books
         }),
-        getBookInfo: build.query<IBookInfo|any, string>({
-            query: (isbn13: string) => ({
-                url: `books/${isbn13}`
-                
-            })
+        getBookInfo: build.query<IBookInfo[], { books: string[] }>({
+            queryFn: async (arg, api, extraOptions, baseQuery) => {
+                const { books } = arg
+                let results = []
+                for (const [_, value] of Object.entries(books)) {
+                    let result = await baseQuery(`/books/${value}`)
+                    results.push(result.data)
+                }
+                return {data: results as IBookInfo[]}
+            }
         }),
     })
         
 })
 
-export const {useSearchBooksQuery, useLazySearchBooksQuery, useLazyGetBookInfoQuery} = itbookApi
+export const {useSearchBooksQuery, useLazySearchNewBooksQuery, useLazyGetBookInfoQuery} = itbookApi
